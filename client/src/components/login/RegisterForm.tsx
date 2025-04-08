5d3b6b1b2021ee6c18fc057885c006e9b41ab172
Juan Jose Figueroa <juanjo.figue97@gmail.com>
2025-04-08 17:17:06 -0500

    Deploy Dashboard

--- client/src/components/login/RegisterForm.tsx
+++ client/src/components/login/RegisterForm.tsx
@@ -1,7 +1,7 @@
 // src/components/RegisterForm.tsx
 import { useState } from "react";
 import axios from "axios";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import Swal from "sweetalert2";
 import logo from "/Eventia.png";
 
@@ -13,8 +13,6 @@ const RegisterForm = () => {
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
-
-  const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -31,7 +29,7 @@ const RegisterForm = () => {
     };
 
     try {
-      const response = await axios.post(
+      await axios.post(
         `${import.meta.env.VITE_API_URL}/auth/register`,
         userData
       );
